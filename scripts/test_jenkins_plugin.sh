4fc41c4e826edab48517f0e41f66b15351260d45
Diego Perini <diego.perini@saucelabs.com>
2022-01-03 15:30:43 +0300

    Adapted CI to test plugin on latest Jenkins

--- scripts/test_jenkins_plugin.sh
+++ scripts/test_jenkins_plugin.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 export TF_API_KEY=$TF_API_KEY
+export TEMP_JENKINS_ADMIN_PASS=`cat /root/.jenkins/secrets/initialAdminPassword`
 
 if [ ! -f $1 ]; then
 	echo "TestFairy plugin hpl file not found at path [$1]."
 	exit 2
 fi
 
-echo "Installing $1"
-ls ~/.jenkins/war/WEB-INF/jenkins-cli.jar
-java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ install-plugin $1
-java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ restart
-sleep 15
-
 # Fail early
 set -e
+
+# Install plugin
+echo "Installing $1"
+ls jenkins-cli.jar
+java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$TEMP_JENKINS_ADMIN_PASS install-plugin file://`realpath $1`
+java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$TEMP_JENKINS_ADMIN_PASS restart
+sleep 15
 
 # Test 1
 bash upload_test/upload_test.sh
